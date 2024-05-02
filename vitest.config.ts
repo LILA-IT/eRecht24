23a6f68b9bab1767e24599c6766fed47935ba77d
Torben Raab <torben.raab@lila-it.de>
2024-05-02 13:19:05 +0200

    ci(vitest): test changes from vitest in ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 		globals: true,
 		setupFiles: ["./src/__tests__/setupVitest.ts"],
 		exclude: ["node_modules", "build", "coverage", "src/api", "lib", "*.d.ts"],
-		reporters: "vitest-sonar-reporter",
+		reporters: ["default", "vitest-sonar-reporter"],
 		outputFile: "sonar-report.xml",
 		coverage: {
 			reporter: ["cobertura", "html-spa", "text", "lcov"],
